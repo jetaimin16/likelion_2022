71a7cbef2b05faecfb47a5da79c487be340f7a8a
jetaimin16 <semin1116@gmail.com>
2022-05-06 18:55:21 +0900

    python advanced 1

--- front-end/1-15.js
+++ front-end/1-15.js
@@ -10,5 +10,32 @@ const sum = (num1, num2) => {
     return num1 + num2;
 }
 
+// 중괄호와 return 생략
+const sum = (num1, num2) => num1 + num2;
+
 const result = sum(10, 20);
 console.log(result);
+
+// 매개변수 하나일 때 소괄호 생략
+const pow = x => x*x;
+
+const result2 = pow(10);
+console.log(result2);
+
+// 매개변수 없을 경우 소괄호만 작성
+const printPie = () => 3.14;
+
+const result3 = printPie();
+console.log(result3);
+
+// 객체 반환할 경우
+const getObject = () => ({
+    name: "철수",
+    age: 20
+});
+
+const obj = getObject();
+console.log(obj.name);
+
+// 내부함수 형태
+const outer = x => () => x * x;
